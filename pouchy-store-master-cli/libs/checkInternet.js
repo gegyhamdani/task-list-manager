d475cc010beda2a379ec1da54d9bd49ac6bb4143
gegyhamdani <gegyhamdani@gmail.com>
2020-08-15 15:01:10 +0700

    chore(deps): add custom pouchy-store and its deps for cli

---
+++ pouchy-store-master-cli/libs/checkInternet.js
@@ -0,0 +1,22 @@
+const TIMEOUT_INTERNET_CHECK = 5; // seconds
+const fetch = require("node-fetch");
+
+const checkInternet = (url) => {
+  return new Promise((resolve, reject) => {
+    const timer = setTimeout(() => {
+      reject(new Error("No internet connection"));
+    }, TIMEOUT_INTERNET_CHECK * 1000);
+
+    fetch(url, { method: "HEAD" })
+      .then(() => {
+        clearTimeout(timer);
+        resolve(true);
+      })
+      .catch(() => {
+        clearTimeout(timer);
+        reject(new Error("No internet connection"));
+      });
+  });
+};
+
+export default checkInternet;
